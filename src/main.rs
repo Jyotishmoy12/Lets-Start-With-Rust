f2e44394f3bc77aa6dc63a20e1ef1af7e7ef8daa
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2025-02-04 21:23:14 +0530

    rust starting project

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::io; // input output library
+
+fn main() {
+    println!("Guess the number");
+
+    println!("Please input your guess.");
+
+    // for storing the user input
+    let mut guess = String::new();
+    // to handle user input the read_line just append it not overwrite it
+    // this returns two possible states (Result) Ok or Err
+    io::stdin().read_line(&mut guess).expect("Failed to read user input");
+
+    println!("You guessed: {guess}");
+}
